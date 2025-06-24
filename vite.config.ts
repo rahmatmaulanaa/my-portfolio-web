13f9a39adf0e4dad9b5af073952039fecfb3ed9d
Rahmat Maulana <rahmatmaulana0220@gmail.com>
2025-06-25 00:45:40 +0800

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // Ganti 'username' dan 'repo-name' sesuai dengan GitHub kamu
 export default defineConfig({
-  base: '/my-portfolio-web/',
+  base: "/my-portfolio-web/",
   plugins: [react()],
 });
