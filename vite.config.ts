29ea428d90a539e0a9216722e28c7937ff367c31
Rahmat Maulana <rahmatmaulana0220@gmail.com>
2025-06-25 00:33:51 +0800

    Setup GitHub Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// Ganti 'username' dan 'repo-name' sesuai dengan GitHub kamu
 export default defineConfig({
+  base: '/my-portfolio-web/',
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
